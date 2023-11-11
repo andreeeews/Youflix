ae4b6b78070d328d58ff410a286a910a7ba9dc5e
Andrews <andreeews2k12@gmail.com>
2023-11-11 13:59:00 +0100

    feat: cors and multer cfg

--- API/config/cors.config.js
+++ API/config/cors.config.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 
 module.exports = cors({
-  origin: process.env.CORS_ORIGIN || "http://127.0.0.1:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true
 })
