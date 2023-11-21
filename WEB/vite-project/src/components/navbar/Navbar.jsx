ab38a465ea9445a4c7d79f9e24964d23898988db
Andrews <andreeews2k12@gmail.com>
2023-11-21 03:41:59 +0100

    minor fix

--- WEB/vite-project/src/components/navbar/Navbar.jsx
+++ WEB/vite-project/src/components/navbar/Navbar.jsx
@@ -80,22 +80,6 @@ function Navbar() {
                       Ajustes
                     </Link>
                   </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Earnings
-                    </a>
-                  </li>
                 </ul>
                 <div className="py-1">
                   <a
